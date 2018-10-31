8e28de9cb5e883178ab6d22d46812fb19753d49d
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-31 14:27:02 -0400

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
+  get    '/about',   to: 'users#rewards'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
