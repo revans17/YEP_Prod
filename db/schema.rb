a6f816c94665274d4cef7ced5c7a6ceaff630183
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-31 17:59:01 -0400

    Updated User Page with Star

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20181026045204) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated", default: true
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
