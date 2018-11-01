c663b82e88482d59852e9d716d49e17fbd5fae8f
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-31 23:33:32 -0400

    updated number of posts per page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,6 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
    before_action :correct_user, only:  :destroy
     before_action :admin_user, only: :destroy
-
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
