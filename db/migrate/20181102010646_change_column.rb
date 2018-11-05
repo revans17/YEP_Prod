99a688410ac2c3b1b9d09c8e0610308427ede32d
Ryan Evans <revans17@masonlive.gmu.edu>
2018-11-04 21:11:34 -0500

    update like button css

---
+++ db/migrate/20181102010646_change_column.rb
@@ -0,0 +1,4 @@
+class ChangeColumn < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101200035) do
+ActiveRecord::Schema.define(version: 20181102010646) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
